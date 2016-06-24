a6ab5189c1d2aaeec23ad84cd73fd9f7a7dc922b
Michael Biczo <mbiczo@arrowheadcu.org>
2016-06-24 09:00:28 -0700

    add load button function

--- lynda/js/ajax.js
+++ lynda/js/ajax.js
@@ -16,52 +16,65 @@ function appendText(text, textElementType, parentElementID) {
 	// Append elements to page
 	ajaxElement.appendChild(ajaxText);
 	parentElement.appendChild(ajaxElement);	
-}  
-
-
-
-// Create a new xhr request object
-var xhrRequest;
-
-// Check Browser Compatibility
-if (window.XMLHttpRequest) {  // firefox, safari, chrome
-	xhrRequest =  new XMLHttpRequest();
-} else {  // older IE
-	xhrRequest = ActiveXObject( "Microsoft.XMLHTTP" );
 }
 
-// Configure request object
-xhrRequest.open("GET", "http://localhost:8000/data/MasterFields.json", true);
 
-// Create handle for response
-xhrRequest.onreadystatechange = function() {
-	// Check if successful
-	if ( (xhrRequest.readyState === 4) && ( xhrRequest.status===200)) {
+// Baic AJAX function
+function loadAJAX (requestObject, outputElementId) {
 	
-		// Append TEXT, to text element, to parent element
-		//appendText(xhrRequest.responseText, "p", "body");
-		
-		// JSON parsing
-		var jsonItems = JSON.parse(xhrRequest.responseText);
-		console.log(jsonItems);
-		
-		// Build output structure
-		var output = '<ul>';
-		for (var key in jsonItems) {
-			output += '<li>' + key + '</li>';
-		}
-		output += '</ul>';
-		
-		// Output to page
-		document.getElementById("jsonOutput").innerHTML = output;
-		
+	// Create a new xhr request object
+	var xhrRequest;
+	//var requestObject = "http://localhost:8000/data/MasterFields.json";
+	//var outputElementId = "jsonOutput";
+
+	// Check Browser Compatibility
+	if (window.XMLHttpRequest) {  // firefox, safari, chrome
+		xhrRequest =  new XMLHttpRequest();
+	} else {  // older IE
+		xhrRequest = ActiveXObject( "Microsoft.XMLHTTP" );
 	}
+
+	// Configure request object
+	xhrRequest.open("GET", requestObject, true);
+
+	// Create handle for response
+	xhrRequest.onreadystatechange = function() {
+		// Check if successful
+		if ( (xhrRequest.readyState === 4) && ( xhrRequest.status===200)) {
+		
+			// Append TEXT, to text element, to parent element
+			//appendText(xhrRequest.responseText, "p", "body");
+			
+			// JSON parsing
+			var jsonItems = JSON.parse(xhrRequest.responseText);
+			console.log(jsonItems);
+			
+			// Build output structure
+			var output = '<ul>';
+			for (var key in jsonItems) {
+				output += '<li>' + key + '</li>';
+			}
+			output += '</ul>';
+			
+			// Output to page
+			document.getElementById(outputElementId).innerHTML = output;
+		}
+	}
+	// Send request object
+	xhrRequest.send();
 }
 
-// Send request object
-xhrRequest.send();
 
+// Get a handle on the event button
+var loadButton = document.getElementById("ajaxLoadButton");
 
+// Set request object and output element
+var requestObject = "http://localhost:8000/data/MasterFields.json";
+var outputElementId = "jsonOutput";
+	
+loadButton.onclick = function () {
+	loadAJAX(requestObject, outputElementId)
+}
 
 
 
