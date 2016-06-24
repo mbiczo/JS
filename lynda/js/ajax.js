e9e020c680b7d9ca84d9befe6de8c2db1510bf41
michael <michaelbiczo@Michaels-MacBook-Pro.local>
2016-06-23 20:38:35 -0700

    add simple working test

--- lynda/js/ajax.js
+++ lynda/js/ajax.js
@@ -2,31 +2,27 @@
 This script is a simple ajax task for testing and learning.
 */
 
-// Make multiple server requests
-for ( x = 0; x < 1000; x++) {
-	
-	// Create a new xhr request object
-	var xhrRequest = new XMLHttpRequest();
+// Create a new xhr request object
+var xhrRequest = new XMLHttpRequest();
 
-	// Configure request object
-	xhrRequest.open('GET', '../data/data.txt', false);
+// Configure request object
+xhrRequest.open('GET', 'http://localhost:8000/data/data.txt', false);
 
-	// Send request object
-	xhrRequest.send();
-	
-	if (xhrRequest.readyState === 4) {
-		// Output the request object and response text
-		console.log(xhrRequest);
-		console.log(xhrRequest.responseText);
+// Send request object
+xhrRequest.send();
+    
+if (xhrRequest.readyState === 4) {
+    // Output the request object and response text
+    console.log(xhrRequest);
+    console.log(xhrRequest.responseText);
 
-		// Get and Create page elements
-		var parentElement = document.getElementById("body");
-		var ajaxElement = document.createElement("p");
-		var ajaxText = document.createTextNode(xhrRequest.responseText);
+    // Get and Create page elements
+    var parentElement = document.getElementById("body");
+    var ajaxElement = document.createElement("p");
+    var ajaxText = document.createTextNode(xhrRequest.responseText);
 
-		// Append elements to page
-		ajaxElement.appendChild(ajaxText);
-		parentElement.appendChild(ajaxElement);
-	}
+    // Append elements to page
+    ajaxElement.appendChild(ajaxText);
+    parentElement.appendChild(ajaxElement);
 }
 
