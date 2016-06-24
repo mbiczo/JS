2e6b75f1d9376a631160a73dafabfad8fe27d7f4
michael <michaelbiczo@Michaels-MacBook-Pro.local>
2016-06-23 21:02:10 -0700

    add appendText function

--- lynda/js/ajax.js
+++ lynda/js/ajax.js
@@ -2,13 +2,11 @@
 This script is a simple ajax task for testing and learning.
 */
 
-
-
 // Appends text to a parent element in desired element type
 function appendText(text, textElementType, parentElementID) {
 	// Output the request object and response text
-	//console.log(xhrRequest);
-	//console.log(xhrRequest.responseText);
+	console.log(xhrRequest);
+	console.log(xhrRequest.responseText);
 
 	// Get and Create page elements
 	var parentElement = document.getElementById(parentElementID);
@@ -17,33 +15,44 @@ function appendText(text, textElementType, parentElementID) {
 
 	// Append elements to page
 	ajaxElement.appendChild(ajaxText);
-	parentElement.appendChild(ajaxElement);
-}   
+	parentElement.appendChild(ajaxElement);	
+}  
+
+
+
 
 // Create a new xhr request object
-var xhrRequest;
-
-// Check ajax browser compatibility
-if ( window.XMLHttpRequest ) {  // firefox, safari, chrome
-	xhrRequest = new XMLHttpRequest();
-} else { // IE (older)
-	xhrRequest = new ActiveXObject( "Microsoft.XMLHTTP" );
-}
+var xhrRequest = new XMLHttpRequest();
 
 // Configure request object
 xhrRequest.open("GET", "http://localhost:8000/data/data.txt", false);
 
-// Create handler for response
-xhrRequest.onstatuschange = function () {
-	
-	// Check if successful
-	if ( (xhrRequest.readyState === 4) && ( request.status===200)) {
-		console.log(xhrRequest);
-		
-		// Append text, text element type, parent element
-		appendText(xhrRequest.responseText, "p", "body");
-	}
-}
-
 // Send request object
 xhrRequest.send();
+
+// Check if successful
+if ( (xhrRequest.readyState === 4) && ( xhrRequest.status===200)) {
+	
+	appendText(xhrRequest.responseText, "p", "body");
+	
+	
+	
+	
+	/*	
+	// Output the request object and response text
+	console.log(xhrRequest);
+	console.log(xhrRequest.responseText);
+
+	// Get and Create page elements
+	var parentElement = document.getElementById("body");
+	var ajaxElement = document.createElement("p");
+	var ajaxText = document.createTextNode(xhrRequest.responseText);
+
+	// Append elements to page
+	ajaxElement.appendChild(ajaxText);
+	parentElement.appendChild(ajaxElement);
+	*/
+	
+	}
+
+
