d7ccd4b6c424f2290a6e7335963000d804bce0c3
Michael Biczo <mbiczo@arrowheadcu.org>
2016-06-24 13:30:59 -0700

    add basic working example of jQuery ajax

--- lynda/js/ajaxJQ.js
+++ lynda/js/ajaxJQ.js
@@ -1,1 +1,14 @@
-$('jsonOutput').load('../data/data.json');
+
+//$('#jsonOutput').load('../data/masterFields.json');
+
+$.getJSON('../data/masterFields.json', function(data) {
+	console.log(data)
+	
+	var output = '<ul>';
+	$.each(data, function(key, value) {
+		output += '<li>' + key + '</li>';
+	});
+	output += '</ul>';
+	
+	$('#jsonOutput').append(output);
+});
