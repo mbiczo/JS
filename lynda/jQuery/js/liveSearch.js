b7075cda7dc045347494627a77faf1d2bcdc4d9e
michael <michaelbiczo@Michaels-MacBook-Pro.local>
2016-06-27 21:41:38 -0700

    present json file with links

--- lynda/jQuery/js/liveSearch.js
+++ lynda/jQuery/js/liveSearch.js
@@ -6,6 +6,33 @@ $("document").ready(function() {
 	// Verify jQuery installed
 	console.log("hello console, I'm installed!");
 	
+	// Run ajax
+	getData();	
+
 });
 
+
+function getData(){
+	var $searchResults = $("#searchResults");
+	
+	$.ajax({
+		url: "http://localhost:8000/jQuery/data/MasterFields.json",
+		type: "GET",
+		dataType: "JSON",
+		success: function(data) {
+			//console.log('Success!', data);
+			$.each(data, function(i, item) {
+				var itemInfo = {'ItemName': i, 'ItemID': item.ID};
+				$searchResults.append("<p class= 'bold'>" + i + "</p>");
+				$searchResults.append("<p>Item Definition: " + "<a href='#' id= " + item.ID +" >" + item.data_governance.item_definition + "</a>" + "</p>");
+				$searchResults.append("<button id= 'changeDef' name= " + item.ID + ">Change Def</button>")
+				//console.log(item);
+				//console.log(itemInfo);
+			});
+		},
+		error: function() {
+			alert("There's been an error");
+		}
+	});
+}
 
