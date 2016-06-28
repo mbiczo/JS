efaba177c20a7ae976c207c9f589a8eab247a36a
michael <michaelbiczo@Michaels-MacBook-Pro.local>
2016-06-27 20:05:02 -0700

    add begining jQuery liveSearch BETA files

---
+++ lynda/jQuery/js/liveSearch.js
@@ -0,0 +1,11 @@
+// liveSearch: simple app that dynamically updates items as the user types
+
+
+$("document").ready(function() {
+	
+	// Verify jQuery installed
+	console.log("hello console, I'm installed!");
+	
+});
+
+
