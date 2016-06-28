5ac31eebdb87af782ceac33961b2df6ba54485a9
michael <michaelbiczo@Michaels-MacBook-Pro.local>
2016-06-27 19:37:08 -0700

    add begining jQuery tutorial files)

--- lynda/jQuery/js/jQueryExamples.js
+++ lynda/jQuery/js/jQueryExamples.js
@@ -1,3 +1,20 @@
 // Simple intro to jQuery
 
-console.log("hello console");
+
+$("document").ready(function() {
+	
+	// Verify jQuery installed
+	console.log("hello console, I'm installed!");
+	
+	// Append to end of body tag
+	$("body").append("<p>Hello There</p>");
+
+	// Select by tag, add css styling to all
+	$("p").css("border", "3px solid red");
+
+	
+
+
+});
+
+
