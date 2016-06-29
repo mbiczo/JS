7cabc0a5ee22aac16ad3412880d5c7fbe15876a7
Michael Biczo <mbiczo@arrowheadcu.org>
2016-06-29 13:32:34 -0700

    add working version of dynamic typing

--- lynda/jQuery/js/liveSearch.js
+++ lynda/jQuery/js/liveSearch.js
@@ -10,46 +10,63 @@ $("document").ready(function() {
 	// Verify jQuery installed
 	console.log("hello console, I'm installed!");
 	
-	// Setup variables
-	var $searchResults = $("#searchResults");
-	var searchField = $('#search').val().replace(/\s/g, "+");  //replace global spaces with '+'
-	var myExp = new RegExp(searchField, "i");  //convert to case-insensitive regex
-	
-	// Ajax function to GET data
-	$.ajax({
-		url: "http://localhost:8000/jQuery/data/MasterFields.json",
-		type: "GET",
-		dataType: "JSON",
-		success: function(data) {
-			
-			// For each item, append to page
-			$.each(data, function(header, item) {
+	// When enduser types into search box, run ajax
+	$('#search').keyup(function() {
+		
+		// Setup variables
+		
+		var $searchResults = $("#searchResults");
+		var searchField = $('#search').val().replace(/\s/g, "+");  //replace global spaces with '+'
+		var myExp = new RegExp(searchField, "i");  //convert to case-insensitive regex
+		
+		// Ajax function to GET data
+		$.ajax({
+			url: "http://localhost:8000/jQuery/data/MasterFields.json",
+			type: "GET",
+			dataType: "JSON",
+			success: function(data) {
 				
-				//var itemInfo = {'ItemName': header, 'ItemID': item.ID};
-				$searchResults.append("<p class= 'bold'>" + header + "</p>");
+				// Create output string of list items
+				var outputList = "";
 				
-				// consider creating a function
-				$searchResults.append(	"<p>Item Definition: " + 
-										"<a href='#' id= " + 
-										item.ID +" >" + 
-										item.data_governance.item_definition + 
-										"</a>" + 
-										"</p>");
+				// For each item, append to page
+				$.each(data, function(header, item) {
+					
+					// Check for matches
+					if ( header.search(myExp) != -1 ) {
+						
+						//var itemInfo = {'ItemName': header, 'ItemID': item.ID};
+						outputList += "<p class= 'bold'>" + header + "</p>";
 
-			}); //for each
+						// consider creating a function
+						outputList +=	"<p>Item Definition: " + 
+												"<a href='#' id= " + 
+												item.ID +" >" + 
+												item.data_governance.item_definition + 
+												"</a>" + 
+												"</p>";
+
+					} // if match
+					
+				}); //for each
+				
+				// replace contents with new list
+				$searchResults.html(outputList);
+				
+			}, // on success
 			
-		}, // on success
-		
-		
-		// Run this when there's an error
-		error: function() {
 			
-			alert("There's been an error");
+			// Run this when there's an error
+			error: function() {
+				
+				alert("There's been an error");
+				
+			}  //on error
 			
-		}  //on error
+		}); //.ajax
+
+	});  //on.keyup
 		
-	}); //.ajax
-	
 }); //document.ready
 
 
