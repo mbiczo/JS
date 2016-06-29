db8683fd06344975cb8561e370a2473c55e5d1c5
Michael Biczo <mbiczo@arrowheadcu.org>
2016-06-29 08:24:07 -0700

    code cleanup

--- lynda/jQuery/js/liveSearch.js
+++ lynda/jQuery/js/liveSearch.js
@@ -1,33 +1,43 @@
 // liveSearch: simple app that dynamically updates items as the user types
 
 
+// Wait until the whole doc is loaded
 $("document").ready(function() {
 	
 	// Verify jQuery installed
 	console.log("hello console, I'm installed!");
 	
-	// Run ajax
-	getData();	
-
+	// Load Data in the background
+	getData();
 });
 
 
+// Ajax function to GET data
 function getData(){
 	var $searchResults = $("#searchResults");
-	
+	var searchField = $('#search').val().replace(/\s/g, "+");  //replace global spaces with '+'
+	var myExp = new RegExp(searchField, "i");  //convert to case-insensitive regex
+		
 	$.ajax({
 		url: "http://localhost:8000/jQuery/data/MasterFields.json",
 		type: "GET",
 		dataType: "JSON",
 		success: function(data) {
-			//console.log('Success!', data);
-			$.each(data, function(i, item) {
-				var itemInfo = {'ItemName': i, 'ItemID': item.ID};
-				$searchResults.append("<p class= 'bold'>" + i + "</p>");
-				$searchResults.append("<p>Item Definition: " + "<a href='#' id= " + item.ID +" >" + item.data_governance.item_definition + "</a>" + "</p>");
-				$searchResults.append("<button id= 'changeDef' name= " + item.ID + ">Change Def</button>")
-				//console.log(item);
-				//console.log(itemInfo);
+			
+			// For each item, append to page
+			$.each(data, function(header, item) {
+				
+				//var itemInfo = {'ItemName': header, 'ItemID': item.ID};
+				$searchResults.append("<p class= 'bold'>" + header + "</p>");
+				
+				// consider creating a function
+				$searchResults.append(	"<p>Item Definition: " + 
+										"<a href='#' id= " + 
+										item.ID +" >" + 
+										item.data_governance.item_definition + 
+										"</a>" + 
+										"</p>");
+
 			});
 		},
 		error: function() {
